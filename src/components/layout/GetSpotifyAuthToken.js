e22d4c85b4996e35b4881c9030ef4551ada38376
Daniel Salvatori <dsalvat596@gmail.com>
2020-04-01 16:50:59 -0400

    serve/build

--- src/components/layout/GetSpotifyAuthToken.js
+++ src/components/layout/GetSpotifyAuthToken.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function GetSpotifyAuthToken(props) {
+  let clientId;
+
+  if (process.env.NODE_ENV !== 'production') {
+    clientId = process.env.REACT_APP_CLIENT_ID;
+  } else {
+    clientId = process.env.CLIENT_ID;
+  }
   const authEndpoint = 'https://accounts.spotify.com/authorize';
-  const clientId = process.env.REACT_APP_CLIENT_ID;
   const redirectUri = 'http://localhost:3000';
   const scopes = 'playlist-modify-public';
 
